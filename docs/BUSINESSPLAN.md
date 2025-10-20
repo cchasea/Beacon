37d0db88017e8b9ff96cbcb47b261adb8d0c7ebd
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-10-20 20:19:20 +0000

    Updating to latest FlutterFlow output. Init commit

--- docs/BUSINESSPLAN.md
+++
@@ -1,1 +0,0 @@
-## [Business Plan Sheet](https://docs.google.com/spreadsheets/d/1tolRtcCXg8bqKK3hNEZT6el8fWGRO_oYfG5IXXBpeDg/edit?usp=sharing)
---
+++ lib/login/login_model.dart
@@ -0,0 +1,30 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'login_widget.dart' show LoginWidget;
+import 'package:flutter/material.dart';
+
+class LoginModel extends FlutterFlowModel<LoginWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for email_textField widget.
+  FocusNode? emailTextFieldFocusNode;
+  TextEditingController? emailTextFieldTextController;
+  String? Function(BuildContext, String?)?
+      emailTextFieldTextControllerValidator;
+  // State field(s) for password_textField widget.
+  FocusNode? passwordTextFieldFocusNode;
+  TextEditingController? passwordTextFieldTextController;
+  String? Function(BuildContext, String?)?
+      passwordTextFieldTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    emailTextFieldFocusNode?.dispose();
+    emailTextFieldTextController?.dispose();
+
+    passwordTextFieldFocusNode?.dispose();
+    passwordTextFieldTextController?.dispose();
+  }
+}
